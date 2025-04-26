dd08a5a8f96c8ad703c576ab6a70ddb4c67375d8
josedomingos919 <josedomingos919@gmail.com>
2025-04-27 00:12:47 +0100

    feat: programacao sequencial

---
+++ LogicaDeProgramacao/src/main/java/com/mycompany/logicadeprogramacao/ProgramacaoSequencial.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.logicadeprogramacao;
+
+/**
+ *
+ * @author User
+ */
+public class ProgramacaoSequencial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Calculo da area
+        double base, area, altura;
+        
+        base = 10;
+        altura = 8;
+        
+        area = base * altura / 2;
+        
+        System.out.println(area);
+    }
+    
+}

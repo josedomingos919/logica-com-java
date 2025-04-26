69395a2c8a0c69fbf368ade313ebdcab329cb054
josedomingos919 <josedomingos919@gmail.com>
2025-04-27 00:27:50 +0100

    feat: programacao etrutura condicional

---
+++ LogicaDeProgramacao/src/main/java/com/mycompany/logicadeprogramacao/OperadoresCondicionais.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.logicadeprogramacao;
+
+/**
+ *
+ * @author User
+ */
+public class OperadoresCondicionais {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Estrutura Condicional
+        
+        int a, b;
+        a = 4;
+        b = 8;
+        
+        System.out.println(a + " > " + b + " = " + (a > b ));
+        
+         System.out.println(a + " > " + b + " || " + b + " == " + a + "  :  " + ( a>b || b==a )  );
+    }
+    
+}

d11a63fbe44bf6e6de789ba81762fa68de894027
josedomingos919 <josedomingos919@gmail.com>
2025-04-27 00:32:16 +0100

    feat: estrutura condicional simples

--- LogicaDeProgramacao/src/main/java/com/mycompany/logicadeprogramacao/EstruturaCondicionalSimples.java
+++ LogicaDeProgramacao/src/main/java/com/mycompany/logicadeprogramacao/EstruturaCondicionalSimples.java
@@ -14,7 +14,11 @@ public class EstruturaCondicionalSimples {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int nota = 2;
+        
+        if(nota >= 5) {
+            System.out.println("Aprovado");
+        }
     }
     
 }

7e1c01290acf3e973214bd80fac0849258084cfb
josedomingos919 <josedomingos919@gmail.com>
2025-04-28 22:36:32 +0100

    feat: estudando vector

---
+++ LogicaDeProgramacao/src/main/java/com/mycompany/logicadeprogramacao/Vetores.java
@@ -0,0 +1,14 @@
+package com.mycompany.logicadeprogramacao;
+
+public class Vetores {
+    public static void main(String[] args) {
+        int valor;
+        int[] dados = new int[5];
+
+        dados[2] = 9;
+        dados[3] = 7;
+        dados[0] = 6;
+
+        System.out.println(dados[3]);
+    }
+}
